1889d0b4d477ad74bf3f5a95765cc0106cf4ebe4
Yegor Cheprasov <yegorcheprasov@gmail.com>
2026-06-29 00:40:10 +0300

    Start awww daemon only if not running

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -11,5 +11,5 @@ hl.on("hyprland.start", function ()
   hl.exec_cmd("wl-paste --type text --watch cliphist store")
   hl.exec_cmd("wl-paste --type image --watch cliphist store")
 
-  hl.exec_cmd("awww-daemon")
+  hl.exec_cmd("pgrep awww-daemon >/dev/null || awww-daemon")
 end)
