d82eac864277d5e99da328a17ceb671edce07cdd
Yegor Cheprasov <yegorcheprasov@gmail.com>
2026-07-03 02:25:28 +0300

    Update theme

--- dot_config/hypr/modules/appearance.lua
+++ dot_config/hypr/modules/appearance.lua
@@ -20,7 +20,7 @@ hl.config({
         rounding_power   = 2,
 
         active_opacity   = 1.0,
-        inactive_opacity = 0.9,
+        inactive_opacity = 1.0,
 
         shadow           = {
             enabled      = true,
--- dot_config/hypr/modules/rules.lua
+++ dot_config/hypr/modules/rules.lua
@@ -26,3 +26,14 @@ hl.window_rule({
     move  = "20 monitor_h-120",
     float = true,
 })
+
+-- Audio popup: pwvucontrol as a compact floating panel under the top-right of the bar
+-- (waybar pulseaudio on-click toggles it). Bar ends at y~=36 (margin-top 6 + height 30).
+hl.window_rule({
+    name  = "pwvucontrol-popup",
+    match = { class = "com.saivert.pwvucontrol" },
+
+    float = true,
+    size  = "460 640",
+    move  = "monitor_w-470 46",
+})
